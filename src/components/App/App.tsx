2285d72ad40ece7fbc9a1fa72a88185c4188e291
Zaman <Sunny.Zaman@ontario.ca>
2020-04-26 16:25:44 -0400

    Added material ui appbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Switch, Route, Redirect, Link } from 'react-router-dom';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import useStyles from './styles'
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+          🍔
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <div>
-        <Link to="/">Home Page</Link>
-        <span> | </span>
-        <Link to="/about">About Page</Link>
+        <div>
+          <Link to="/">Home Page</Link>
+          <span> | </span>
+          <Link to="/about">About Page</Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home Page</h1>
+          </Route>
+          <Route exact path="/about">
+            <h1>About Page</h1>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <h1>Home Page</h1>
-        </Route>
-        <Route exact path="/about">
-          <h1>About Page</h1>
-        </Route>
-        <Redirect to="/"/>
-      </Switch>
     </div>
   );
 }
